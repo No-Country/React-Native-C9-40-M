f94e35514e3d380b93c34692f9091ab078e99975
Yuliana Boglione <yulyana77@hotmail.com>
2023-02-06 10:32:54 -0300

    cambios de yuli

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 import {
   Button,
   View,
@@ -7,12 +7,12 @@ import {
   Image,
   ActivityIndicator,
   Text,
-} from 'react-native';
+} from "react-native";
 
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
 
-import miImagen from '../assets/images/home1.jpg';
-import { UserContext } from '../GlobalStates/userContext';
+import miImagen from "../assets/images/home1.jpg";
+import { UserContext } from "../GlobalStates/userContext";
 
 export function HomeScreen() {
   const navigation = useNavigation();
@@ -34,11 +34,11 @@ export function HomeScreen() {
         <View style={styles.buttonContainer}>
           <Button
             onPress={() => {
-              navigation.navigate('Landing');
+              navigation.navigate("Landing");
             }}
             title="Cerrar Sesión"
             color="purple"
-            accessibilityLabel="Learn more about this purple button"
+            accessibilityLabel="Learn more about this"
           />
         </View>
       </View>
@@ -49,15 +49,15 @@ export function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   title: {
     fontSize: 22,
-    textAlign: 'center',
+    textAlign: "center",
   },
   image: {
-    width: '100%',
+    width: "100%",
     height: 256,
     marginBottom: 20,
   },
