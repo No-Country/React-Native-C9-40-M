8dfdfeb219b8eb8f4ed526dca7126f780990cca1
Yuliana Boglione <yulyana77@hotmail.com>
2023-02-16 18:33:47 -0300

    agregue uso de useUpdateUser

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,16 +1,16 @@
-import { StyleSheet } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { HomeScreen } from '../screens/Home';
-import { LoginScreen } from '../screens/Login';
-import { RegisterScreen } from '../screens/Register';
-import { ResetPasswordScreen } from '../screens/ResetPassword';
-import { ForgotPasswordScreen } from '../screens/ForgotPassword';
-import { ConfirmEmailScreen } from '../screens/ConfirmEmail';
-import { LandingScreen } from '../screens/Landing';
-import { JobsScreen } from '../screens/Jobs';
-import ProfileAdd from '../screens/ProfileAdd';
+import { NavigationContainer } from "@react-navigation/native";
+import { HomeScreen } from "../screens/Home";
+import { LoginScreen } from "../screens/Login";
+import { RegisterScreen } from "../screens/Register";
+import { ResetPasswordScreen } from "../screens/ResetPassword";
+import { ForgotPasswordScreen } from "../screens/ForgotPassword";
+import { ConfirmEmailScreen } from "../screens/ConfirmEmail";
+import { LandingScreen } from "../screens/Landing";
+import { JobsScreen } from "../screens/Jobs";
+import ProfileAdd from "../screens/ProfileAdd";
 
 const Stack = createNativeStackNavigator();
 
@@ -21,34 +21,34 @@ export const Navigation = (props: Props) => {
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{ headerShown: false }}
-        // initialRouteName={"Profile"}
+        initialRouteName={"Profile"}
       >
         <Stack.Screen
           name="Landing"
           component={LandingScreen}
-          options={{ title: 'Welcome to FindYourDreamJob' }}
+          options={{ title: "Welcome to FindYourDreamJob" }}
         />
         <Stack.Screen
           name="Jobs"
           component={JobsScreen}
-          options={{ title: 'Encuentra tu trabajo ideal' }}
+          options={{ title: "Encuentra tu trabajo ideal" }}
         />
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{ title: 'Home' }}
+          options={{ title: "Home" }}
         />
         <Stack.Screen
           name="Login"
           component={LoginScreen}
-          options={{ title: '', headerShown: false }}
+          options={{ title: "", headerShown: false }}
         />
         <Stack.Screen name="ConfirmEmail" component={ConfirmEmailScreen} />
         <Stack.Screen name="ResetPassword" component={ResetPasswordScreen} />
         <Stack.Screen
           name="Register"
           component={RegisterScreen}
-          options={{ title: '', headerShown: false }}
+          options={{ title: "", headerShown: false }}
         />
         <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
         <Stack.Screen name="Profile" component={ProfileAdd} />
